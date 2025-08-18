c5999ca6738d470b10969a665ade12de2d4f7a23
angel-iscoding <angel.iscoding@gmail.com>
2025-08-18 18:19:24 -0500

    first commit

---
+++ src/main/java/com/alura/challenge/foro/PracticandoSpringFrameworkChallengeForoHubApplication.java
@@ -0,0 +1,13 @@
+package com.alura.challenge.foro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticandoSpringFrameworkChallengeForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticandoSpringFrameworkChallengeForoHubApplication.class, args);
+	}
+
+}
